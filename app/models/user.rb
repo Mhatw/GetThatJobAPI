bc5ad6c54f9427344f5cf343efa49cb7234c5bb9
Alexis Parave <aparavev@gmail.com>
2022-07-12 17:20:52 -0500

    FEAT: add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # Validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+
+  # Associations
   belongs_to :userable, polymorphic: true
   has_secure_token
   has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_180427) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_220626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_180427) do
     t.string "linkedin_url"
     t.text "experience"
     t.text "education"
-    t.bigint "profession_id", null: false
+    t.bigint "profession_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["profession_id"], name: "index_professionals_on_profession_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_180427) do
     t.bigint "userable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["userable_type", "userable_id"], name: "index_users_on_userable"
   end
