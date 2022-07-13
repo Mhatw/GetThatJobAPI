2166b889b1e1acabaaed0d7edc6d833675aa2b2c
Alexis Parave <aparavev@gmail.com>
2022-07-12 22:57:45 -0500

    FIX: requirement model

--- db/migrate/20220713034916_create_requirements.rb
+++ db/migrate/20220713034916_create_requirements.rb
@@ -1,8 +1,9 @@
 class CreateRequirements < ActiveRecord::Migration[7.0]
   def change
     create_table :requirements do |t|
-      t.text :requirement
+      t.text :content
       t.boolean :mandatory
+      t.references :job, null: false, foreign_key: true
 
       t.timestamps
     end
