4f3ac042a1cf8222c8d5f109aa5decaf4f87d148
Alexis Parave <aparavev@gmail.com>
2022-07-14 18:12:42 -0500

    CHORE: update validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
   # Associations
   has_many :jobs, dependent: :nullify
-  
+
   # Validations
-  validates :name, inclusion: { in: %w(Manufacturing Legal Education Goverment Sales Tech)}
 end
