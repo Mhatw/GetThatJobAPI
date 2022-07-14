60de92f255bc9a842423ce30b6b971772e3cabe9
Alexis Parave <aparavev@gmail.com>
2022-07-13 23:30:58 -0500

    FEAT: create professional controller and add create method

---
+++ app/controllers/professionals_controller.rb
@@ -0,0 +1,18 @@
+class ProfessionalsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    professional = Professional.new(professional_params)
+    if professional.save
+      render json: professional, status: :created
+    else
+      render json: { errors: professional.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def professional_params
+    params.require(:professional).permit(:name, :phone, :birth_date, :linkedin_url, :experience, :education, :profession_id)
+  end
+end
