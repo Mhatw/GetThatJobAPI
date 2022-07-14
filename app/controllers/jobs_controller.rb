0c3ba8f128704d54417ab5d2988f57e047bf76a7
Alexis Parave <aparavev@gmail.com>
2022-07-14 15:25:17 -0500

    FEAT: create a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,19 @@ class JobsController < ApplicationController
     render json: jobs
   end
 
-  # def create
+  def create
+    job = Job.new(job_params)
+    job.company = current_user.userable
+    if job.save
+      render json: job, status: :created
+    else
+      render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
-  # end
+  private
+
+  def job_params
+    params.require(:job).permit(:name, :description, :salary_min, :salary_max, :type_id, :category_id)
+  end
 end
