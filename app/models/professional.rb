517eb563f106b28038cc53f89bb13779e7d993f3
Frank Cano <frank.cano@utec.edu.pe>
2022-07-15 11:15:38 -0500

    FIX: phone validations to professional

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -6,10 +6,17 @@ class Professional < ApplicationRecord
   has_many :jobs, through: :applications
 
   # validates
-  PHONE_REGEX = /\+[0-9]{6,13}/
+  PHONE_REGEX = /\A\+[0-9]{6,13}\Z/
   validates :name, presence: true
   validates :phone, format: { with: PHONE_REGEX, message: "+[country code][number]" }
+  validate :phone_number
   validate :birth
+
+  def phone_number
+    unless phone.is_a?(String)
+      render json: error, message: "+[country code][number]"
+    end
+  end
 
   def birth
     if birth_date > Time.zone.today
