704c00c9ef7a775c2fc3b4353404b6457394fb04
Alexis Parave <aparavev@gmail.com>
2022-07-21 00:00:49 -0500

    REFACTOR: validations to permit to create a company and a professional without data

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,25 +1,17 @@
 class Professional < ApplicationRecord
   # associations
-  belongs_to :profession
+  belongs_to :profession, optional: true
   has_one :user, as: :userable, dependent: :destroy
   has_many :applications, dependent: :destroy
   has_many :jobs, through: :applications
 
   # validates
   PHONE_REGEX = /\A\+[0-9]{6,13}\Z/
-  validates :name, presence: true
-  validates :phone, format: { with: PHONE_REGEX, message: "+[country code][number]" }
-  validate :phone_number
+  validates :phone, format: { with: PHONE_REGEX, message: "+[country code][number]" }, allow_nil: true
   validate :birth
 
-  def phone_number
-    unless phone.is_a?(String)
-      render json: error, message: "+[country code][number]"
-    end
-  end
-
   def birth
-    if birth_date > Time.zone.today
+    if birth_date && birth_date > Time.zone.today
       errors.add(:birth_date, "can´t be in the future")
     end
   end
