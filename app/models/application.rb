9621851a2b49cd3ad1c128e531139feb8dd6c6db
Alexis Parave <aparavev@gmail.com>
2022-07-21 13:43:01 -0500

    FEAT: add unique index to applications table to have a unique combination between job_id and professioanl_id, in this way a professional only can apply one time to a job

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,9 @@
 class Application < ApplicationRecord
+  # Associations
   belongs_to :job
   belongs_to :professional
   belongs_to :status
+
+  # Validations
+  validates :job_id, uniqueness: { scope: :professional_id, message: "already applied" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_15_184123) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_171153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_184123) do
     t.bigint "status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["job_id", "professional_id"], name: "index_applications_on_job_id_and_professional_id", unique: true
     t.index ["job_id"], name: "index_applications_on_job_id"
     t.index ["professional_id"], name: "index_applications_on_professional_id"
     t.index ["status_id"], name: "index_applications_on_status_id"
